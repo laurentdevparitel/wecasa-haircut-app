8206454872d69f596c4f87a6577f4b17f348b322
laurentdevparitel <laurent.dev.paritel@gmail.com>
2018-12-23 18:34:42 +0100

    added Navigation & Routing

---
+++ src/Navigation.js
@@ -0,0 +1,39 @@
+
+export const Navigation = [
+  {
+    path: "/",
+    title: "Home",
+    view: "Home",
+    slug: "home"
+  },
+  {
+    path: "/catalog",
+    title: "Catalog",
+    view: "Catalog",
+    slug: "catalog"
+  },
+  {
+    path: "/address-selection",
+    title: "Address selection",
+    view: "AddressForm",
+    slug: "address-selection"
+  },
+  {
+    path: "/appointment-selection",
+    title: "Appointment selection",
+    view: "AppointmentForm",
+    slug: "appointment-selection"
+  },
+  {
+    path: "/booking-summary",
+    title: "Booking Summary",
+    view: "BookingSummary",
+    slug: "booking-summary"
+  },
+  {
+    path: "/confirmation",
+    title: "Confirmation",
+    view: "Confirmation",
+    slug: "confirmation"
+  }
+];
